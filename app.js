e9caab68c2c0ca028631c3177528c0d2b23e1a42
atakanhbk <atakanhbk@gmail.com>
2023-10-18 11:49:36 +0300

    Add Card and CSS them

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const container = document.createElement("div");
 const row = document.createElement("div");
 const createTaskCard = document.createElement("div");
+const taskCardList = document.createElement("div");
 
 const taskTitleText = document.createElement("h2");
 const taskDescText = document.createElement("h2");
@@ -11,9 +12,16 @@ const taskCreateButton = document.createElement("button");
 
 function ControlStyle() {
   container.className = "container";
+
   row.className = "row";
   row.style.display = "flex";
   row.style.justifyContent = "center";
+
+  taskCardList.className = "task-card-list";
+  taskCardList.style.display = "flex";
+  taskCardList.style.flexWrap =  "wrap";
+  taskCardList.style.gap = "20px";
+  taskCardList.style.marginTop = "20px";
 
   createTaskCard.className = "create-task-card";
   createTaskCard.style.border = "2px solid black";
@@ -40,10 +48,15 @@ function ControlStyle() {
   taskDescText.innerHTML = "Description";
 
   taskDescInput.style.maxWidth = "200px";
+  taskDescInput.className = "task-desc-input";
+
+  taskTitleInput.className = "task-title-input";
 }
+
 function AppendChild() {
   document.body.appendChild(container);
   container.appendChild(row);
+  container.appendChild(taskCardList);
   row.appendChild(createTaskCard);
   createTaskCard.appendChild(taskTitleText);
   createTaskCard.appendChild(taskTitleInput);
@@ -52,5 +65,29 @@ function AppendChild() {
   createTaskCard.appendChild(taskCreateButton);
 }
 
+function AddEventListener() {
+  taskCreateButton.addEventListener("click", function () {
+    CreateTask(taskTitleInput.value, taskDescInput.value);
+  });
+}
+
+function CreateTask(taskTitle, taskDesc) {
+  const toDoInfo = {
+    taskTitle,
+    taskDesc,
+  };
+
+  TaskCard();
+}
+
+function TaskCard() {
+  const originialDiv = document.getElementsByClassName("create-task-card");
+  const fakeDiv = originialDiv[0].cloneNode(true);
+
+  taskCardList.appendChild(fakeDiv);
+}
+
+
 AppendChild();
 ControlStyle();
+AddEventListener();
